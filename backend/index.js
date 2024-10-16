eea3c07042815e901ae3c68c953238863c48e9b2
pushkarbanjare <pushkarbanjaree@gmail.com>
2024-10-17 00:19:51 +0530

    Backend done

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./db/connectDB.js"; // '.js' bcz this is "type":"module"
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // middleware
+app.use(cookieParser()); // allows to parse incoming cookies
+
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
